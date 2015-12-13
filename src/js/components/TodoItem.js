78bd5d93a7a1f0e387f92ad19afb17b076e29516
Lucas <lucas@agtlucas.com>
2015-12-13 09:13:52 -0200

    Update TodoItem

--- src/js/components/TodoItem.js
+++ src/js/components/TodoItem.js
@@ -9,4 +9,59 @@ export default class TodoItem extends React.Component {
       editText = ''
     }
   }
+
+  render() {
+    if (this.state.editing) {
+      return(
+        <div className="todo__item editing">
+          <input
+            ref="edit__input"
+            onChange={this._onChange}
+            onKeyDown={this._onKeyDown}
+            value={this.state.editText}
+            />
+          <a className={save} onClick={this._stopEdit}><i className="icon__submit" /></a>
+        </div>
+      );
+    }
+
+    return(
+      <div className="todo__item">
+        <div className="item__text">
+          {this.props.item.text}
+          <div className="options">
+            <a onClick={this._startEdit}><i className="icon__edit" /></a>
+            <a onClick={this._removeItem}><i className="icon__delete" /></a>
+          </div>
+        </div>
+        <div className="item__date">
+          <PrettyDate value={this.props.item.createdAt} />
+        </div>
+      </div>
+    );
+  }
+
+  _startEdit() {
+    this.setState({
+      editText: this.props.item.text,
+      editing: true
+    }, () => {
+      let node = this.refs.edit__input.getDOMNode();
+      node.focus();
+      let len = this.state.editText.length;
+      node.setSelectionRange(len, len);
+    });
+  }
+
+  _onChange(e) {
+    this.setState({
+      editText: e.target.value
+    });
+  }
+
+  _onKeyDown(e) {
+    if (e.keyCode === 13) {
+      this._stopEdit();
+    }
+  }
 }
