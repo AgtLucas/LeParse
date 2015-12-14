7af86c5c60170dfc8abb842559826124ddbef9c5
Lucas <lucas@agtlucas.com>
2015-12-13 23:25:40 -0200

    Add index_sample

---
+++ src/js/index_sample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Parse from 'parse';
+import style from '../css/style.css'
+
+Parse.initialize()
+
+import TodoList from './components/TodoList';
+
+render(
+  <TodoList />,
+  document.getElementById('root')
+);
