6c5e9473718a465516c7f53a4aeab5c31585ef9e
Lucas <lucas@agtlucas.com>
2015-12-13 23:22:32 -0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Parse from 'parse';
 import style from '../css/style.css'
 
-class App extends React.Component {
-  render() {
-    return(
-      <div>
-        <h1 className={style.wtf}>Hello App!</h1>
-      </div>
-    );
-  }
-}
+Parse.initialize()
+
+import TodoList from './components/TodoList';
 
 render(
-  <App />,
+  <TodoList />,
   document.getElementById('root')
 );
