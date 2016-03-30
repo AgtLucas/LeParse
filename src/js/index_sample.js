3816feb3aff2bb1297c781536cee6bbc8d751eaf
Lucas <AgtLucas@users.noreply.github.com>
2016-03-30 16:13:26 -0300

    Update index_sample.js

--- src/js/index_sample.js
+++ src/js/index_sample.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Parse from 'parse';
+import React from 'react'
+import { render } from 'react-dom'
+import Parse from 'parse'
 import style from '../css/style.css'
 
 Parse.initialize()
 
-import TodoList from './components/TodoList';
+import TodoList from './components/TodoList'
 
 render(
   <TodoList />,
   document.getElementById('root')
-);
+)
